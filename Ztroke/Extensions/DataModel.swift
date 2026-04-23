5ab49e1bd31efe81af4febc5ebffb8f0b7c9fb9f
Sande Effendi <sande.effendi@gmail.com>
2026-04-23 17:37:16 +0700

    refactor

--- Ztroke/Extensions/DataModel.swift
+++ Ztroke/Extensions/DataModel.swift
@@ -5,24 +5,69 @@
 //  Created by Zulkifli on 22/04/26.
 //
 
-import SwiftUI
+import Foundation
 
-struct HomeCard  {
-    
-    let id: UUID = UUID()
-    let date: String
-    let duration: String
-    let swingCount: Int
-    let correct: Int
-    let incorrect: Int
-}
+enum StrokeType: String, CaseIterable, Identifiable, Hashable {
+    case forehand
+    case backhand
 
-struct DataModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    var id: String { rawValue }
+
+    var title: String {
+        rawValue.capitalized
+    }
+
+    var symbolName: String {
+        "figure.badminton"
+    }
+
+    var mirrored: Bool {
+        self == .backhand
     }
 }
 
-#Preview {
-    DataModel()
+struct PracticeSetup: Hashable {
+    let stroke: StrokeType
+    let durationMinutes: Int
+}
+
+struct PracticeSession: Identifiable, Hashable {
+    let id: UUID
+    let date: Date
+    let stroke: StrokeType
+    let durationMinutes: Int
+    let swingCount: Int
+    let correctCount: Int
+
+    var incorrectCount: Int {
+        max(swingCount - correctCount, 0)
+    }
+
+    var accuracy: Double {
+        guard swingCount > 0 else { return 0 }
+        return Double(correctCount) / Double(swingCount)
+    }
+}
+
+struct PracticeOverview {
+    let sessionCount: Int
+    let totalMinutes: Int
+    let totalSwings: Int
+    let correctSwings: Int
+
+    var incorrectSwings: Int {
+        max(totalSwings - correctSwings, 0)
+    }
+
+    var accuracyRate: Double {
+        guard totalSwings > 0 else { return 0 }
+        return Double(correctSwings) / Double(totalSwings)
+    }
+}
+
+enum AppRoute: Hashable {
+    case history
+    case cameraSetup(StrokeType)
+    case practice(PracticeSetup)
+    case practiceSummary(UUID)
 }
