fcc7eed32c602f62a89bafa75f911e407c87045e
Zulkifli <zulkiflibasri8@gmail.com>
2026-04-26 21:32:51 +0700

    SwingFiturCompleted

---
+++ Ztroke/Features/Home/Views/CameraSetUp.swift
@@ -0,0 +1,18 @@
+//
+//  CameraSetUp.swift
+//  Ztroke
+//
+//  Created by Zulkifli on 23/04/26.
+//
+
+import SwiftUI
+
+struct CameraSetUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraSetUp()
+}
